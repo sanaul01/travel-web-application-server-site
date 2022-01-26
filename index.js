8b1c8decfa435b1ae8e7dab65958c62186498fee
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-27 01:34:51 +0600

    post data to serber

--- index.js
+++ index.js
@@ -6,22 +6,31 @@ const { MongoClient } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hcshw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, });
 
-
 async function run() {
-    try {
+  try {
         await client.connect();
-        console.log("successfully");
-    } 
-    finally {
-    //   await client.close();
-    }
-}
-run().catch(console.dir);
+        const database = client.db("travel_blogs");
+        const blogsCollection = database.collection("blogs");
 
+    app.post("/blogs", async (req, res) => {
+        const blog = req.body;
+        const result = await blogsCollection.insertOne(blog);
+        console.log(result);
+        res.json(result);
+    });
+
+  } 
+  finally {
+    //   await client.close();
+  }
+
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Travelers!");
